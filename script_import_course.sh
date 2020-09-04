ce5f2f5ec8a2e1912eadac8252e11b08d09f43f0
aureliencroq <aurelien.croq@themoocagency.com>
2020-09-04 15:57:04 +0200

    Fix import courses

--- script_import_course.sh
+++ script_import_course.sh
@@ -16,10 +16,10 @@ echo "***********************Starting course import script**********************
 
 
 #Token for authentication, using a session already pending
-sessionid=""
-csrftoken=""
-sessionid2=""
-
+sessionid="xxxxxxxxxxxxxxxxxxxx:xxxx:xxxxx"
+csrftoken="xxxxx"
+fqdn="studio.xxxxx"
+directory_of_exports="/xxxx/xxxx/xxxx"
 
 #Check if folder named "courses" exist and go inside, if not exit with error cide
 if [ -d "./courses" ]
@@ -32,20 +32,37 @@ else
     exit 9999 # die with error code 9999
 fi
 
+i=1
 #Looping trough the each file, get their id by their name and use curl on endpoint edx provide
 for file in ./*.tar.gz
 do
-
   #Spliting the filename to get only id
   filename="${file##*/}"
   echo "Starting import of $filename"
-  IFS=':'
-  read -a strarr <<< "$filename"
   filenametrimed="$(echo $filename | cut -f 1 -d '.')"
-  
-curl 'https://studio.preprod.amundiacademy.com/import/'$filenametrimed  -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Origin: https://studio.preprod.amundiacademy.com' -H 'X-CSRFToken: SufhxSryaj52tPa5bgawvX9jEWd5k92v' -H 'X-Requested-With: XMLHttpRequest' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36' -H 'Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryu8pRtChbOypKCMie' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-Fetch-Mode: cors' -H 'Referer: https://studio.preprod.amundiacademy.com/import/'$filenametrimed  -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7' -H 'Cookie:' --data-binary $'------WebKitFormBoundaryu8pRtChbOypKCMie\r\nContent-Disposition: form-data; name="course-data"; filename=$filename\r\nContent-Type: application/gzip\r\n\r\n\r\n------WebKitFormBoundaryu8pRtChbOypKCMie--\r\n' --compressed
 
-  echo "$filename is imported"
+curl 'https://'$fqdn'/import/'$filenametrimed \
+  -H 'Connection: keep-alive' \
+  -H 'Pragma: no-cache' \
+  -H 'Cache-Control: no-cache' \
+  -H 'Accept: application/json, text/javascript, */*; q=0.01' \
+  -H 'X-CSRFToken: '$csrftoken \
+  -H 'X-Requested-With: XMLHttpRequest' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36' \
+  -H 'Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryUotv1wxUKMTtjbxM' \
+  -H 'Origin: https://'$fqdn \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-Mode: cors' \
+  -H 'Sec-Fetch-Dest: empty' \
+  -H 'Referer: https://'$fqdn'/import/'$filenametrimed \
+  -H 'Accept-Language: fr-FR,fr;q=0.9,cs;q=0.8,en-US;q=0.7,en;q=0.6,de;q=0.5' \
+  -H 'Cookie: csrftoken='$csrftoken'; edxloggedin=true; experiments_is_enterprise=false; sessionid='$sessionid'; openedx-language-preference=en' \
+  -F 'course-data=@'$directory_of_exports$filename \
+  --compressed
+
+  echo "$i -- $filename is imported"
+  i=$((i+1))
+  sleep 3s
 done
 
 echo "**********************************End of course(s) import**********************************"
